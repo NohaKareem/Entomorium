1714f3ccbde9185bbe5d5158e5003685538fafa5
Noha Kareem <NohaKareem@users.noreply.github.com>
2020-11-25 16:28:03 -0500

    Added hero section animation

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@
 	const DOWN_SCROLL = document.querySelector('.downScroll');
 	const BUGS = document.querySelectorAll('.bug');
 	const BUG_TITLES = document.querySelectorAll('.bug h4');
+	const LOGO = document.querySelectorAll('.logo');
 	let currTitle;
 
 	// scrollto animation
@@ -33,4 +34,13 @@
 			}
 		});
 	});
+
+	let timeline = gsap.timeline();
+	let easeFunction = "slow(0.7, 0.7, false)";
+	timeline
+		.from(".logo", 1.5, { autoAlpha: 0, ease: easeFunction })
+		.from(".hero p", 1.5, { autoAlpha: 0, ease: easeFunction })
+		.from(".hero img", 1.5, { autoAlpha: 0, ease: easeFunction });
+
+	// gsap.from(LOGO, 7, { autoAlpha: 0, ease: "slow(0.7, 0.7, false)" });
 })();
