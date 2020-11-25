acaae061cac19aff7e389af5d92bd78408ca8dcd
Noha Kareem <NohaKareem@users.noreply.github.com>
2020-11-25 15:59:16 -0500

    Added hover effects

--- js/main.js
+++ js/main.js
@@ -1,75 +1,36 @@
 (_ => {
 	"use strict";
 
-	const VID = document.querySelector('video');
-	const PLAY_BTN = document.querySelector('.play');
-	const SPEED_BTN = document.querySelector('.speed');
-	const STOP_BTN = document.querySelector('.stop');
-	const SCRUB_SLIDER = document.querySelector('.scrubSlider');
-	const SCRUB_PROGRESS = document.querySelector('.seen');
-	const SCRUB_MAX = SCRUB_SLIDER.max;
-	const TIME_LAPSED = document.querySelector('.scrubberCon span');
-	const PLAY_FA = `<i class="fa fa-play" aria-hidden="true"></i>`;
-	const PAUSE_FA = `<i class="fa fa-pause" aria-hidden="true"></i>`;
-	let speed = 1;
+	const DOWN_SCROLL = document.querySelector('.downScroll');
+	const BUGS = document.querySelectorAll('.bug');
+	const BUG_TITLES = document.querySelectorAll('.bug h4');
+	let currTitle;
 
-	VID.removeAttribute('controls');
-
-	// update button display
-	let updateButton = (btn, html) => { btn.innerHTML = html; }
-	
-	// pad input with leading zero
-	let padZeros = num => { return `${num}`.padStart(2, '0'); }
-
-	// toggle play pause 
-	PLAY_BTN.addEventListener('click', _ => {
-		if(VID.paused) {
-			updateButton(PLAY_BTN, PAUSE_FA);
-			VID.play();
-		} else {
-			updateButton(PLAY_BTN, PLAY_FA);
-			VID.pause();
-		}
+	// scrollto animation
+	DOWN_SCROLL.addEventListener("click", e => {
+		TweenLite.to(window, 1.5, {
+			scrollTo: {
+				y: ".menu", 
+				offsetY: 20, 
+				autoKill: false, 
+				ease: "slow(0.7, 0.7, false)"
+			}
+		});
 	});
 
-	// modify speed
-	SPEED_BTN.addEventListener('click', _ => {
-		speed = (speed + 0.25) % 2;
-		speed = speed > 0 ? speed : 2;
-		SPEED_BTN.innerHTML = `${speed}X`;
-		VID.playbackRate = speed;
-	}); 
+	// alter hover text for bugs coming soon
+	BUGS.forEach((bug, i) => {
+		bug.addEventListener("mouseover", _=> {
+			if (!bug.classList.contains('activeOption')) {
+				currTitle = BUG_TITLES[i].innerHTML;
+				BUG_TITLES[i].innerHTML = "Coming Soon";
+			}
+		});
 
-	// stop video event handler
-	let stopVid = _ => {
-		VID.pause();
-		VID.currentTime = 0; 
-		updateButton(PLAY_BTN, PLAY_FA);
-	}
-
-	// stop on click, or auto-stop on complete
-	STOP_BTN.addEventListener('click', stopVid);
-	VID.addEventListener('ended', stopVid);
-
-	// get scrub value
-	let getScrubValue = _ => { return (SCRUB_MAX * VID.currentTime) / VID.duration; }
-
-	// update scrub progress
-	let updateScrubProgress = _ =>  { SCRUB_PROGRESS.style.width = `${getScrubValue()}%`; }
-
-	// update scrubber time and position
-	VID.addEventListener('timeupdate', e => {
-		SCRUB_SLIDER.value = getScrubValue();
-		updateScrubProgress();
-		
-		let minutes = Math.floor(VID.currentTime / 60);
-		let seconds = Math.floor(VID.currentTime - minutes * 60);
-		TIME_LAPSED.innerHTML = `${padZeros(minutes)}:${padZeros(seconds)}`;
-	});
-	
-	// update scrub slider on input 
-	SCRUB_SLIDER.addEventListener('input', e => {
-		VID.currentTime = e.currentTarget.value * VID.duration / SCRUB_MAX;
-		updateScrubProgress();
+		bug.addEventListener("mouseout", _=> {
+			if (!bug.classList.contains('activeOption')) { 
+				BUG_TITLES[i].innerHTML = currTitle;
+			}
+		});
 	});
 })();
